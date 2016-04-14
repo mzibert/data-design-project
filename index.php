6f09925ed329f2a46209573e9d2069b313e21836
merri zibert <mzibert@cnm.edu>
2016-04-14 11:20:35 -0600

    created a table

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1> Persona</h1>
 		<h2> Name:</h2>
-		<img src="images/profilepic.jpg" alt="profilepic"/>
+		<img src="images/profilepic.jpg" alt="profilepic" height="300" width="250"/>
 		Janet Nardi
 		<h2>Age:</h2>
 		mid 50’s
@@ -48,13 +48,42 @@
 		<p>Janet has a BIG meeting coming up with Squaw Valley Resort Ski Resort (home of the 1960
 			Olympics).  There will be 6 executives attending the meeting, one will be the executive
 			chef.  Janet needs to find a fantastic restaurant to take the executives too off property
-			so she is sure to have their uninterrupted attention.  Because chef is also attending she
+			so she is sure to have their uninterrupted attention.  Because chef is also attending, she
 			needs the food to be spectacular and the wine to be even better.  She is on the plane
 			traveling from New York to Reno so she wants to research all of the local restaurants so
 			she can be sure that she selects one that will meet her standards and impress chef.
 			She lands at 5:15pm and the meeting is scheduled at 6:00.  She needs to be able to book
 			the place for a party of 7 and send an email with all of the details (including address
 			details) to the 6 executives using the plane wifi.</p>
+		<h1>Conceptual Schema</h1>
+		<table>Entities
+			<tr>
+				<th>Profile</th>
+				<th>Review</th>
+				<th>Ratings</th>
+			</tr>
+			<tr>
+				<td>profileId</td>
+				<td>userName</td>
+				<td>userSalt</td>
+				<td>userHash</td>
+				<td>primaryDiningCity</td>
+			</tr>
+			<tr>
+				<td>userName</td>
+				<td>review</td>
+				<td>reviewTags</td>
+				<td>null</td>
+				<td>null</td>
+			</tr>
+			<tr>
+				<td>food</td>
+				<td>ambience</td>
+				<td>service</td>
+				<td>value</td>
+				<td>null</td>
+			</tr>
+		</table>
 	</body>
 </html>
 
