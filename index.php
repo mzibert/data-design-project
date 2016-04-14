510faa559e535b4c754eabe58a8b9284a6b05967
merri zibert <mzibert@cnm.edu>
2016-04-14 13:59:51 -0600

    woking on revising the use case

--- index.php
+++ index.php
@@ -45,16 +45,16 @@
 			needs to include free form text so that individual reviewers can type their experiences
 			andopinions.</p>
 		<h1> Use Case</h1>
-		<p>Janet has a BIG meeting coming up with Squaw Valley Resort Ski Resort (home of the 1960
-			Olympics).  There will be 6 executives attending the meeting, one will be the executive
-			chef.  Janet needs to find a fantastic restaurant to take the executives too off property
-			so she is sure to have their uninterrupted attention.  Because chef is also attending, she
-			needs the food to be spectacular and the wine to be even better.  She is on the plane
-			traveling from New York to Reno so she wants to research all of the local restaurants so
-			she can be sure that she selects one that will meet her standards and impress chef.
-			She lands at 5:15pm and the meeting is scheduled at 6:00.  She needs to be able to book
-			the place for a party of 7 and send an email with all of the details (including address
-			details) to the 6 executives using the plane wifi.</p>
+		<p> Goal:  Janet wants to use opentable to read reviews of the restaurant she has chosen for her business dinner
+			meeting.
+		<ol>
+			<li>0)  Janet goes to www.opentable.com and finds the list of restaurant reviews she wants to read regarding
+				her restaurant of choice for her business dinner meeting on her Lenovo Think Pad while flying the friendly
+				skies</li>
+			<li>1)open table generates the list of user reviews </li>
+		</ol>
+
+		</p>
 		<h1>Conceptual Schema</h1>
 		<table>Entities
 			<tr>
