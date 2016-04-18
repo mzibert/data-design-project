ebb1f0e7de4f819c2f25ede6a8ea561d041ac113
merri zibert <mzibert@cnm.edu>
2016-04-18 16:09:21 -0600

    working on cleaning up syntax errors

---
+++ opentable.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS ratings;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE profile (
+profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+userName VARCHAR(128) NOT NULL,
+userSalt CHAR(64) NOT NULL,
+userHash CHAR(128) NOT NULL,
+PRIMARY KEY (profileId)
+);
+
+CREATE TABLE restaurants(
+restId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+restName VARCHAR(128) NOT NULL,
+PRIMARY KEY (restId)
+);
+
+CREATE TABLE tags(
+tagsId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+tagsName VARCHAR(64),
+FOREIGN KEY (profileId) REFERENCES profile(profileId),
+FOREIGN KEY (restId) REFERENCES restaurants(restId),
+PRIMARY KEY (tagsId)
+);
+
+CREATE TABLE ratings (
+rateService INT UNSIGNED MIN(1) MAX(5),
+rateFood INT  UNSIGNED MIN(1) MAX(5),
+rateAmbience INT UNSIGNED MIN(1) MAX(5),
+rateValue INT UNSIGNED MIN(1) MAX(5),
+FOREIGN KEY (restId) REFERENCES restaurants(restId),
+FOREIGN KEY (profileId) REFERENCES profile(profileId),
+PRIMARY KEY (restId, profileId)
+);
+
+CREATE TABLE reviews (
+tagName VARCHAR (64),
+FOREIGN KEY (profileId) REFERENCES profile (profileId),
+FOREIGN KEY (restId) REFERENCES restaurants (restId),
+PRIMARY KEY (profileId, restId)
+);
