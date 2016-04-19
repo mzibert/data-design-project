e5277b521a40a2e36075c37dd21743e2d6262e11
merri zibert <mzibert@cnm.edu>
2016-04-19 17:00:27 -0600

    created the docblock introducing the project

--- php/Profile.php
+++ php/Profile.php
@@ -2,7 +2,30 @@
 namespace Edu\Cnm\mzibert\DataDesign;
 
 /**
- * accessor method for profile id
+ * First pass at creating an accessor method for my autotroph entity
  *
- * 
- */
+ * @author Merri Zibert <mzibert@cnm.edu>
+ * @version 1.0
+ **/
+class profile implements \JsonSerializable {
+}
+/**
+ * id for this profile that is assigned by the system; this is the primary key
+ * @var int $profileId
+ **/
+private $profileId;
+/**
+ * userName for the person that created this profile
+ * @var string $userName
+ **/
+private $userName;
+/**
+ * userHash hashing the user input
+ * @var string userHash
+ **/
+private $userHash
+/**
+ * userSalt salting the user input
+ * @var string
+ **/
+private $userSalt
